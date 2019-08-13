3a8dc57ff80bd3138ddf66bdbc5fb2c1d57db84e
FredLavoie <flavoie3@gmail.com>
2019-08-13 13:31:21 -0400

    Add 'program' to static directory in server and revise views

--- server/express_server.js
+++ server/express_server.js
@@ -4,18 +4,20 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const app         = express();
 const PORT        = process.env.PORT || 8080; // default port 8080
+const fs          = require('fs');
 
 /************************************* SET / USE / LISTEN *********************************************/
 /*****************************************************************************************************/
 
 app.set('view engine', 'ejs');
 app.set('views','./public/views');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
+app.use(express.static('program'));
 
 app.listen(PORT, () => {
-	console.log(`structural_analysis_program app listening on port ${PORT}!`);
+	console.log(`structural_analysis app listening on port ${PORT}!`);
 });
 
 /**************************************** GET REQUEST ************************************************/
@@ -34,6 +36,13 @@ app.get('/', (req, res) => {
 
 app.post('/submit', (req, res) => {
   console.log('this is req.body: ', req.body);
-  res.send('This is a test!');
+  
+  fs.writeFile('program/data_in.txt', 'utf-8', function(error, data) {
+    if (error) {
+      console.log(error);
+    }
+  });
+      
+  res.render('results');
+  
 });
-

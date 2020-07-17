de6fd75e97ae1f5c73939d5cba2477bed5d81311
FredLavoie <flavoie3@gmail.com>
2020-07-17 11:39:25 -0400

    Revise joint and properties input fields

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
-//***************************** DOCUMENT READY ********************************/
-//*****************************************************************************/
+//************************************** DOCUMENT READY ******************************************/
+//************************************************************************************************/
 
 $(document).ready(function() {
   $('#input-numJoints').change(function() {
@@ -44,26 +44,30 @@ $(document).ready(function() {
   });
 });
   
-//******************************* FUNCTIONS ***********************************/
-//*****************************************************************************/
+//**************************************** FUNCTIONS *********************************************/
+//************************************************************************************************/
 
 // Generate number of input boxes based on numJoints
 function generateJointInput(i) {
-  $('#joint-container').append($('<label>').text(`Joint # ${i}`));
+  $('#joint-container').append($('<label>').text(`Joint # ${i} (X-coord, Y-coord, X-fixed, Y-fixed, Rot-fixed)`));
   
   // create all tags to be appended and assign the values where needed
-  let $jointDiv = $('<div>').addClass('flex-container');
+  let $jointDiv = $('<div>').addClass('joints-container1');
+  let $jointDiv2 = $('<div>').addClass('joints-container2');
   let $coordInput = $('<input>').addClass('input-style form-control').attr('name', 'Joints');
   let $xRest = $('<input>').attr('type', 'checkbox').attr('name', 'Joints');
   let $yRest = $('<input>').attr('type', 'checkbox').attr('name', 'Joints');
   let $rotRest = $('<input>').attr('type', 'checkbox').attr('name', 'Joints');
 
   // append all tags in reverse order (starting with furthest nested tags)
-  $jointDiv
+  $jointDiv2
     .append($coordInput)
     .append($xRest)
     .append($yRest)
     .append($rotRest);
+  
+  $jointDiv
+    .append($jointDiv2);
 
   $('#joint-container')
     .append($jointDiv);
@@ -98,22 +102,22 @@ function generateJointInput(i) {
 function generateEMInput(i) {
   $('#em-container').append($('<label>').text(`Elastic Modulus # ${i}`));
   $('#em-container').append($('<input>')
-      .addClass('input-style form-control')
-      .attr('name', 'ElasticMods'));
+    .addClass('input-style form-control')
+    .attr('name', 'ElasticMods'));
 }
 
 // Generate number of input boxes based on numAreas
 function generateAreaInput(i) {
   $('#area-container').append($('<label>').text(`Area # ${i}`));
   $('#area-container').append($('<input>')
-      .addClass('input-style form-control')
-      .attr('name', 'Areas'));
+    .addClass('input-style form-control')
+    .attr('name', 'Areas'));
 }
 
 // Generate number of input boxes based on numMOIs
 function generateMOIInput(i) {
   $('#moi-container').append($('<label>').text(`Moment of Inertia # ${i}`));
   $('#moi-container').append($('<input>')
-      .addClass('input-style form-control')
-      .attr('name', 'MoI'));
+    .addClass('input-style form-control')
+    .attr('name', 'MoI'));
 }

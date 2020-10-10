92f44920751eb1a52e5eb9f1a924f3308f599772
FredLavoie <flavoie3@gmail.com>
2020-10-10 17:33:45 -0400

    Finish drawMemberPointLoad function

--- public/scripts/draw-functions.js
+++ public/scripts/draw-functions.js
@@ -391,7 +391,6 @@ export function drawMJointLoad(jointNum, moment, globalNodeObject) {
 }
 
 export function drawMemberPointLoad(memberNum, offset, load, globalNodeObject, globalMemberObject) {
-  console.log('globalMemberObject: ', globalMemberObject);
   const startJoint = globalMemberObject[memberNum].joints[0];
   const endJoint = globalMemberObject[memberNum].joints[1];
   const xDist = globalNodeObject[endJoint][0][0] - globalNodeObject[startJoint][0][0];
@@ -405,31 +404,36 @@ export function drawMemberPointLoad(memberNum, offset, load, globalNodeObject, g
   const arrowPointX = globalNodeObject[startJoint][1][0] + relLoadPositionX;
   const arrowPointY = globalNodeObject[startJoint][1][1] + relLoadPositionY;
 
-  // if(load > 0) {
-  //   const base1 = 
-  //   const base2 = 
-  // } else if(load > 0) {
-  //   const base1 = 
-  //   const base2 = 
-  // }
-
-  // const point = `${arrowPointX} ${arrowPointY}`;
-
-
-  console.log('memberLength: ', memberLength);
+  const point = `${arrowPointX} ${arrowPointY}`;
+  const base1 = load < 0 ? `${arrowPointX + 3.5} ${arrowPointY - 10}`: `${arrowPointX + 3.5} ${arrowPointY + 10}`;
+  const base2 = load < 0 ? `${arrowPointX - 3.5} ${arrowPointY - 10}`: `${arrowPointX - 3.5} ${arrowPointY + 10}`;
+  const rotateAngle = load < 0 ? globalMemberObject[memberNum].forceAngle * -1 : 360 - globalMemberObject[memberNum].forceAngle;
 
   const ns = 'http://www.w3.org/2000/svg';
   const box = document.querySelector('#structure-window');
-  const arrow = document.createElementNS(ns, 'circle');
+  const arrow = document.createElementNS(ns, 'polygon');
   arrow.setAttributeNS(null, 'id','member-load');
-  arrow.setAttributeNS(null, 'r', '5');
   arrow.setAttributeNS(null, 'stroke', '#0000ff');
   arrow.setAttributeNS(null, 'fill', '#0000ff');
-  arrow.setAttributeNS(null, 'cx', `${arrowPointX}`);
-  arrow.setAttributeNS(null, 'cy',`${arrowPointY}`);
+  arrow.setAttributeNS(null, 'points', `${point}, ${base1}, ${base2}`);
+  arrow.setAttributeNS(null, 'transform', `rotate(${rotateAngle} ${arrowPointX} ${arrowPointY})`);
   box.append(arrow);
 
+  const headX = arrowPointX;
+  const headY = arrowPointY;
+  const tailX = headX;
+  const tailY = load < 0 ? headY - 50 : headY + 50;
 
+  const line = document.createElementNS(ns, 'line');
+  line.setAttributeNS(null, 'id','member-load');
+  line.setAttributeNS(null, 'stroke', '#0000ff');
+  line.setAttributeNS(null, 'stroke-width', '1');
+  line.setAttributeNS(null, 'x1', `${headX}`);
+  line.setAttributeNS(null, 'y1',`${headY}`);
+  line.setAttributeNS(null, 'x2', `${tailX}`);
+  line.setAttributeNS(null, 'y2',`${tailY}`);
+  line.setAttributeNS(null, 'transform', `rotate(${rotateAngle} ${arrowPointX} ${arrowPointY})`);
+  box.append(line);
 }
 
 // export function drawMemberUDLLoad(memberNum, udl) {

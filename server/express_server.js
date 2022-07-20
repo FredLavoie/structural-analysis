95381885352286c506916c63b21bf7bd18ccbc9f
Fred Lavoie <flavoie3@gmail.com>
2022-07-20 08:12:50 -0400

    Fix a relative path in server file

--- server/express_server.js
+++ server/express_server.js
@@ -13,7 +13,7 @@ import { createInputString } from './lib/create-input-string.js';
 /*********************************************************************************************************************/
 
 app.set('view engine', 'ejs');
-app.set('views', 'src/public/views');
+app.set('views', 'public/views');
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
@@ -38,15 +38,15 @@ app.get('/documentation', (req, res) => {
 });
 
 app.get('/error-exec', (req, res) => {
-  console.log('There was an error parsing the data in the output JSON file');
+  console.error('There was an error parsing the data in the output JSON file');
   res.render('error-exec');
 });
 
 app.get('/results-json', (req, res) => {
   readFile('program/data_out.json', 'utf-8', (error, data) => {
     if (error) {
-      console.log(error);
-      console.log('There was an error reading the output JSON file');
+      console.error('There was an error reading the output JSON file');
+      console.error(error);
       res.render('error-exec');
       return;
     }
@@ -76,16 +76,16 @@ app.post('/results', (req, res) => {
   // code to write to the input file
   writeFile('program/data_in.txt', dataString, (error) => {
     if (error) {
-      console.log('There was an error writing the input file');
-      console.log(error);
+      console.error('There was an error writing the input file');
+      console.error(error);
       res.render('error-write');
       return;
     }
     // code to run executable, then render 'results' page
     execFile('program/sa-linux-exec', (error) => {
       if (error) {
-        console.log('There was an error running the executable');
-        console.log(error);
+        console.error('There was an error running the executable');
+        console.error(error);
         res.render('error-exec');
         return;
       }
